b2a0beb4a55277ec723b9a34113cd633c62bbf6d
Max Brockman <maxbrockman453@gmail.com>
2015-02-20 10:48:12 -0800

    added initial build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,30 @@
-var EmberProject = window.EmberProject = Ember.Application.create();
+var RocknrollcallYeoman = window.RocknrollcallYeoman = Ember.Application.create();
+
+RocknrollcallYeoman.applicationName = "Rock'n'Roll Call";
+
+RocknrollcallYeoman.dummySearchResultsArtists = [
+{
+    id: 1,
+    name: 'Tom Waits',
+    nickname: 'Tommy',
+    type: 'artist',
+    enid: 'ARERLPG1187FB3BB39'
+}, {
+    id: 2,
+    name: 'Thomas Alan Waits',
+    type: 'artist',
+    enid: 'ARERLPG1187FB3BB39'
+}, {
+    id: 3,
+    name: 'Tom Waits w/ Keith Richards',
+    type: 'artist',
+    enid: 'ARMPVNN13CA39CF8FC'
+}];
+
+
+
+
+
 
 /* Order and include as you please. */
 require('scripts/controllers/*');
@@ -8,3 +34,6 @@ require('scripts/routes/*');
 require('scripts/components/*');
 require('scripts/views/*');
 require('scripts/router');
+
+
+
